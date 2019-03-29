a0278b601113a4cf42951d55ce8775d9fe51102e
刘树生 <liushusheng@zhexinit.com>
2019-03-29 16:11:53 +0800

    同步地址 正式

--- src/main/java/com/zhexinit/xuelele/controller/XueLeLeController.java
+++ src/main/java/com/zhexinit/xuelele/controller/XueLeLeController.java
@@ -1,17 +1,16 @@
 package com.zhexinit.xuelele.controller;
 
 import com.zhexinit.xuelele.model.Error;
+import com.zhexinit.xuelele.model.GuaranteeslipByCode;
 import com.zhexinit.xuelele.model.ShouQi;
+import com.zhexinit.xuelele.model.ShouXianShouQi;
 import com.zhexinit.xuelele.utils.JackJson;
 import lombok.Data;
 import org.mongodb.morphia.Datastore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -75,9 +74,8 @@ public class XueLeLeController {
     private Map fetch(Param param,int page){
         MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
         System.out.println("startdate:" + param.getStartDate() + ",enddate:" + param.getEndDate() + ",page:" + page);
-        String url = "http://test-zj.sxfwxt.com:8099/kpireportForm/getGuaranteeslipKpiList";
+        String url = "http://test-zj.sxfwxt.com:8099/lifeInsurance/getGuaranteeslipList";
         try {
-            System.out.println(1/0);
             HttpHeaders headers = new HttpHeaders();
 
             headers.setAccept(new ArrayList<MediaType>() {{
@@ -87,7 +85,7 @@ public class XueLeLeController {
             headers.set("X-Requested-With", "XMLHttpRequest");
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-            paramMap.add("parmData", "{\"orgCode\":null,\"orgCode1\":null,\"orgCode2\":null,\"orgCode3\":null,\"companyCode\":null,\"type\":" + param.getType() + ",\"startDate\":\"" + param.getStartDate() + "\",\"endDate\":\"" + param.getEndDate() + "\",\"customerName\":\"\",\"userName\":\"\",\"dataType\":\"1\",\"proxyCompanyCode\":2,\"authOrgCode\":\"\"}");
+            paramMap.add("parmData", "{\"companyCode\":null,\"slipOrCastSlipCode\":\"\",\"customerName\":\"\",\"userName\":\"\",\"flowState\":\"0\",\"startDate\":\""+param.getStartDate()+"\",\"endDate\":\""+param.getEndDate()+"\",\"proxyCompanyCode\":2,\"authOrgCode\":\"\",\"pagesize\":1}");
             paramMap.add("page", page + "");
             paramMap.add("start", ((page - 1) * limit) + "");
             paramMap.add("limit", limit + "");
@@ -97,7 +95,9 @@ public class XueLeLeController {
             List<Map> list = (List)respMap.get("data");
             for(Map m:list) {
                 String str = JackJson.fromObjectToJson(m);
-                datastore.save(JackJson.fromJsonToObject(str, ShouQi.class));
+                ShouXianShouQi shouXianShouQi = JackJson.fromJsonToObject(str, ShouXianShouQi.class);
+                detail(shouXianShouQi);
+//                datastore.save(shouXianShouQi);
             }
             return respMap;
         }catch (Exception ex){
@@ -111,6 +111,41 @@ public class XueLeLeController {
         return null;
     }
 
+
+
+    private Map detail(ShouXianShouQi shouXianShouQi){
+        MultiValueMap<String, String> paramMap = new LinkedMultiValueMap<>();
+        System.out.println("CastSlipCode:" + shouXianShouQi.getCastSlipCode() );
+        String url = "http://test-zj.sxfwxt.com:8099/lifeInsurance/getGuaranteeslipByCode/2/" + shouXianShouQi.getCastSlipCode();
+        try {
+            HttpHeaders headers = new HttpHeaders();
+
+            headers.setAccept(new ArrayList<MediaType>() {{
+                add(MediaType.APPLICATION_JSON_UTF8);
+            }});
+
+            headers.set("X-Requested-With", "XMLHttpRequest");
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(paramMap, headers);
+            ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.GET,request,Map.class);
+            Map respMap = response.getBody();
+            Map m = (Map)respMap.get("data");
+            String str = JackJson.fromObjectToJson(m);
+            GuaranteeslipByCode guaranteeslipByCode = JackJson.fromJsonToObject(str, GuaranteeslipByCode.class);
+            shouXianShouQi.setGuaranteeslipByCode(guaranteeslipByCode);
+            datastore.save(shouXianShouQi);
+            return respMap;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            Error error = new Error();
+            error.setParam("CastSlipCode:" + shouXianShouQi.getCastSlipCode() );
+            error.setUrl(url);
+            datastore.save(error);
+            System.out.println("CastSlipCode:" + shouXianShouQi.getCastSlipCode() + " 出错");
+        }
+        return null;
+    }
     @Data
     class Param{
         private String type;
@@ -135,4 +170,12 @@ public class XueLeLeController {
         }
     }
 
+    public static void main(String[] args) {
+        String str = "proxyCompanyCode: null,orgType: 0,authOrgCode: null,createTime: 2019-03-20 09:00:04.0,updateTime: 2019-03-20 09:00:04.0,flag: null,page: 1,start: 0,limit: 15,castSlipCode: SX151829581,slipCode: ,companyCode: null,insuranceName: 工银安盛人寿安康e生医疗保险,insuranceCode: MMD,insuranceMoney: 2000000.0000,timeLimit: 一年,paymentTimeLimit: 1年交,isSingleRow: 0,slipYear: 1,formalitiesFee: null,commissionFee: null,insurancePremium: 418.0000,otherPremium: 0.0000,guaranteeFee: 41.8000,feeProportion: 0.0800,fee: 33.4400,commissionProportion: 0.0500,commission: 20.9000,ageIndex: 0,settleAccounts: 0,subtotal: 418.0000";
+        String[] tmp1 = str.split(",");
+        for(String t:tmp1){
+            String s = t.split(":")[0];
+            System.out.println("private String " + s + ";");
+        }
+    }
 }
---
+++ src/main/java/com/zhexinit/xuelele/model/Product.java
@@ -0,0 +1,39 @@
+package com.zhexinit.xuelele.model;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String proxyCompanyCode;
+    private String orgType;
+    private String authOrgCode;
+    private String createTime;
+    private String updateTime;
+    private String flag;
+    private String page;
+    private String start;
+    private String limit;
+    private String castSlipCode;
+    private String slipCode;
+    private String companyCode;
+    private String insuranceName;
+    private String insuranceCode;
+    private String insuranceMoney;
+    private String timeLimit;
+    private String paymentTimeLimit;
+    private String isSingleRow;
+    private String slipYear;
+    private String formalitiesFee;
+    private String commissionFee;
+    private String insurancePremium;
+    private String otherPremium;
+    private String guaranteeFee;
+    private String feeProportion;
+    private String fee;
+    private String commissionProportion;
+    private String commission;
+    private String ageIndex;
+    private String settleAccounts;
+    private String subtotal;
+
+}
